bf7c4c248f01848646809c421ec11fdecf1fd509
Paul  Caruana <201901806@itptstudent.co.uk>
2020-02-06 15:23:58 +0000

    Dice game complete

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1;
+var randomNumber2;
+randomNumber1=Math.floor(Math.random() * 6) + 1;
+randomNumber2=Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('.img1').setAttribute("src","images/dice"+ randomNumber1 +".png");
+document.querySelector('.img2').setAttribute("src","images/dice"+ randomNumber2 +".png");
+
+
+  var result;
+if (randomNumber1 < randomNumber2) {
+  result = "Player 2 Wins!";
+}
+ else if (randomNumber2 < randomNumber1) {
+  result = "Player 1 Wins!";
+}
+ else {
+  result = "Draw";
+}
+  document.querySelector('h1').innerHTML=(result);
